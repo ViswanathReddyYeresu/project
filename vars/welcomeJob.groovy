dfd9aab9c5eeb23afebd16fd184cbc7e52ee9470
ViswanathReddyYeresu <51593553+ViswanathReddyYeresu@users.noreply.github.com>
2021-07-21 18:09:47 +0530

    Update welcomeJob.groovy
    Adding stages to the code

--- vars/welcomeJob.groovy
+++ vars/welcomeJob.groovy
@@ -1,3 +1,14 @@
 def call(String name = 'User') {
+ node {
+
+    def mvnHome = tool 'MAVEN_HOME/'
+    stage ("checkout")  {
+  
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'eee1ec03-c3de-4f05-b42a-f2693ac3fee0', url: 'https://github.com/ViswanathReddyYeresu/hello-world.git']]])
+    }
+    stage ('build')  {
+        sh "${mvnHome}/bin/mvn clean install"
+    }
+}
  echo "Welcome, ${name}."
 }
